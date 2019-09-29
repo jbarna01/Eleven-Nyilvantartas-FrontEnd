bb6adf3f4dc3c6e682e6afcd027c3a497f6475b5
Barna József <jbarna@vipmail.hu>
2019-09-29 22:35:23 +0200

    Ha nincs belépve senki, akkor a Login oldal jelenik meg.

--- src/app/components/hallgatok/hallgatok.component.ts
+++ src/app/components/hallgatok/hallgatok.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {GlobalsService} from "../../services/globals.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-hallgatok',
@@ -11,10 +12,15 @@ export class HallgatokComponent implements OnInit {
   isBelepve: boolean;
 
 
-  constructor(private global: GlobalsService) { }
+  constructor(private global: GlobalsService,
+              private router: Router) { }
 
   ngOnInit() {
     this.global.isBelepve.subscribe(isBelepve => this.isBelepve = isBelepve)
+    if (!this.isBelepve) {
+      console.log('Nincs belépve senki');
+      this.router.navigate(['']);
+    }
   }
 
 }
--- src/app/components/operatorok/operatorok.component.ts
+++ src/app/components/operatorok/operatorok.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {Operator} from "../../models/Operator";
 import {OperatorService} from "../../services/operator.service";
 import {CoreService} from "../../services/core.service";
+import {GlobalsService} from "../../services/globals.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-operatorok',
@@ -11,13 +13,22 @@ import {CoreService} from "../../services/core.service";
 export class OperatorokComponent implements OnInit {
 
   operatorokLista: Operator[];
+  isBelepve: boolean;
 
-  constructor(private operatorService: OperatorService) { }
+  constructor(private operatorService: OperatorService,
+              private global: GlobalsService,
+              private router: Router) { }
 
   ngOnInit() {
-    this.operatorService.getOperatorok().subscribe( operatorok => {
-      this.operatorokLista = operatorok;
-    })
+    this.global.isBelepve.subscribe(isBelepve => this.isBelepve = isBelepve)
+    if (!this.isBelepve) {
+      console.log('Nincs belépve senki');
+      this.router.navigate(['']);
+    } else {
+      this.operatorService.getOperatorok().subscribe(operatorok => {
+        this.operatorokLista = operatorok;
+      })
+    }
   }
 
 }
