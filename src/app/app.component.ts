eee9d044714394008ab10bebbed74bf331b10bc9
Barna József <jbarna@vipmail.hu>
2019-10-23 23:42:11 +0200

    hu-HU

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import huHU from '@translations/hu-HU.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+  constructor(
+    private translateService: TranslateService
+  ) {}
+  ngOnInit() {
+    this.translateService.setTranslation('hu-HU', huHU, true);
+  }
+
+}
+
+
+
--- src/app/components/cimsor/cimsor.component.ts
+++ src/app/components/cimsor/cimsor.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from "@angular/router";
 import {GlobalsService} from "../../services/globals.service";
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-cimsor',
